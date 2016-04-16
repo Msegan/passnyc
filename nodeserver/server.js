6d197b2fbd8b1a7f1a2c590aa98050b1cc00303f
Max Segan <maxsegan@fb.com>
2016-04-16 12:05:59 -0400

    Switch to express

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Ryan\n');
-}).listen(3001);
-console.log('Server running at http://localhost:3001/');
+var express = require('express');
+var app = express();
+
+app.use(function (req, res, next) {
+  console.log('here is where I would log shit');
+  next();
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
